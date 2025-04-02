4a8961f1f93f18885ef2cf73e2c612efacc290a0
gontier <alexis.gontier03@laposte.net>
2025-04-02 16:59:25 +0200

    Add user redirection to dashboard in AuthLayout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { getUser } from '@/lib/auth-session'
+import { redirect } from 'next/navigation';
 
-export default function AuthLayout({
+export default async function AuthLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+
+    const user = await getUser()
+    if (user) {
+        redirect('/dashboard');
+    }
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             {children}
